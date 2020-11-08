dfc066a8e5e67d858fe4591d4d112ce15e746612
Maxim Konstantinov <konstantinovmax@gmail.com>
2020-11-08 13:26:45 +0300

    fix: внести правки в проект

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 //Задаем параллакс эффект для фона
-const parrallaxBackground = document.querySelector('.root');
+/* const parrallaxBackground = document.querySelector('.root');
 parrallaxBackground.addEventListener('mousemove', function(e) {
     const x = e.clientX / window.innerWidth;
     const y = e.clientY / window.innerHeight;  
     parrallaxBackground.style.transform = 'translate(-' + x * 5 + 'px, -' + y * 5 + 'px)';
-});
+}); */

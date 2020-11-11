ff49de116567a9e29f08fa1f31537b668f6f90fd
Maxim Konstantinov <konstantinovmax@gmail.com>
2020-11-11 21:09:21 +0300

    Refactor: переписать код в декларативном стиле

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer className="footer">
-            <p className="footer__copyright">&COPY; Маршак, 2020.</p>
+            <p className="footer__copyright">© Маршак, 2020.</p>
             <p className="footer__author">Сделано студентами <span className="footer__span"><a className="footer__author-link" href="#">Яндекс.Практикум</a></span></p>
         </footer>
     );

5f9cf0820997b2df4a47680a3de0453e3f871144
PavelEremeev <pe19950520@yandex.ru>
2020-11-13 23:24:46 +0300

    Unfortunally trying to make work this validation

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -11,27 +11,34 @@ const useValidation = (value, validations) => {
     const [isFullNameError, setFullNameError] = useState(false)
     const [inputValid, setInputValid] = useState(false)
 
+
     useEffect(() => {
         for (const validation in validations) {
             switch (validation) {
                 case 'minLength':
                     value.length < validations[validation] ? setMinLengthError(true) : setMinLengthError(false)
+
                     break;
                 case 'isEmpty':
                     value ? setEmpty(false) : setEmpty(true)
+
                     break;
                 case 'maxLength':
                     value.length > validations[validation] ? setMaxLengthError(true) : setMaxLengthError(false)
+
                     break;
                 case 'isEmail':
                     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
                         .test(String(value).toLowerCase()) ? setEmailError(false) : setEmailError(true)
+
                     break
                 case 'isContainNumbers':
                     /([0-9])+/g.test(String(value)) ? setNumberError(false) : setNumberError(true)
+
                     break
                 case 'isFullName':
-                    /^[A-ЯЁ][а-яё]+\s[A-ЯЁ][а-яё]+$/.test(String(value)) ? setFullNameError(false) : setFullNameError(true)
+                    /^[A-ЯЁ][а-яё]+\s[A-ЯЁ][а-яё]+$/.test(String(value)) ? setFullNameError(true) : setFullNameError(false)
+
                     break
 
             }
@@ -52,6 +59,8 @@ const useValidation = (value, validations) => {
         isContainNumbersError,
         isFullNameError])
 
+
+
     return {
         isEmpty,
         minLengthError,
@@ -59,8 +68,10 @@ const useValidation = (value, validations) => {
         isEmailError,
         isContainNumbersError,
         isFullNameError,
-        inputValid
+        inputValid,
+
     }
+
 }
 
 
@@ -68,7 +79,7 @@ const useInput = (initialValue, validations) => {
     const [value, setValue] = useState(initialValue)
     const [isDirty, setDirty] = useState(false)
     const valid = useValidation(value, validations)
-
+    console.log(valid)
     const onChange = (e) => {
         setValue(e.target.value)
     }
@@ -86,19 +97,22 @@ const useInput = (initialValue, validations) => {
     }
 }
 
-function ContactForm() {
+
+function ContactForm(inputValid) {
 
     const fullName = useInput('', { isEmpty: true, minLength: 5, isFullName: true })
     const email = useInput('', { isEmpty: true, isEmail: true, maxLength: 40 })
     const phoneNumber = useInput('', { isEmpty: true, isContainNumbers: true, maxLength: 12 })
     const poems = useInput('', { isEmpty: true, minLength: 2 })
 
+
     return (
         <article className="form">
             <h2 className="form__title">ФОРМА.</h2>
             <h3 className="form__subtitle">Заполняя эту форму, вы становитесь частью проекта.</h3>
             <form name="form-turbina" className="form__container" action="#">
-                <input className="form__input" onChange={e => fullName.onChange(e)} onBlur={e => fullName.onBlur(e)} value={fullName.value} name="fullName" type="text" placeholder="Имя и фамилия автора" />
+                <input className="form__input" onChange={e => fullName.onChange(e)} onBlur={e => fullName.onBlur(e)} value={fullName.value} name="fullName" type="text" placeholder="Имя и фамилия автора"
+                />
                 {(fullName.isDirty && fullName.isFullNameError && fullName.isEmpty) && <div style={{ color: 'red' }}>Поле обязательно для заполнения (В формате: А-я)</div>}
                 {(fullName.isDirty && fullName.minLengthError) && <div style={{ color: 'red' }}>Минимальная длина: 5 символов</div>}
                 <input className="form__input" onChange={phoneNumber.onChange} onBlur={phoneNumber.onBlur} value={phoneNumber.value} name="phoneNumber" type="tel" placeholder="Телефон в формате +7 (999) 999 99 99" />
@@ -113,7 +127,7 @@ function ContactForm() {
                 {(poems.isDirty && poems.isEmpty) && <div style={{ color: 'red' }}>Поле обязательно для заполнения</div>}
                 {(poems.isDirty && poems.minLengthError) && <div style={{ color: 'red' }}>Минимальная длина: 2 символов</div>}
                 <div className="form__radio-container">
-                    <input className="form__radio" type="radio" value="agree" />
+                    <input className="form__radio" type="radio" value="agree" required />
                     <span className="form__radio-offer">Согласен с <a className="form__radio-offer-link" href="#">офертой</a></span>
                 </div>
                 <button disabled={!fullName.inputValid || !phoneNumber.inputValid || !email.inputValid || !poems.inputValid} className="form__submit-button">Отправить форму</button>
